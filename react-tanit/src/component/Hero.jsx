8a5833011675802e2d6a80101e9b1fc28e26243b
sahnoune2 <anankarta@gmail.com>
2025-10-08 02:01:34 +0100

    final

--- react-tanit/src/component/Hero.jsx
+++ react-tanit/src/component/Hero.jsx
@@ -58,7 +58,7 @@ export const Hero = () => {
           </p>
           <div className="mt-5 flex items-center justify-center gap-x-6">
             <Link
-              to={"/jobs"}
+              to={"/offers"}
               className="rounded-md bg-sky-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400"
               rel="noreferrer"
             >
